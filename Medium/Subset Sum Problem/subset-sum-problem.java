9f263cec78923a16a729f6160ed0a9058fbbadb5
Parshva Choradia <112927478+Parsh092@users.noreply.github.com>
2023-07-16 20:17:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subset Sum Problem/subset-sum-problem.java
+++ Medium/Subset Sum Problem/subset-sum-problem.java
@@ -34,9 +34,9 @@ class GFG
 //User function Template for Java
 
 class Solution{
-    static boolean f(int i,int[] arr,int target,int[][] dp){
-        if(target==0) return true;
-        if(i==0) return arr[i]==target;
+    static Boolean f(int i,int[] arr,int target,int[][] dp){
+        if(target == 0) return true;
+        if(i==0) return arr[i]==target?true:false;
         if(dp[i][target]!=-1) return dp[i][target]==1?true:false;
         boolean nottake = f(i-1,arr,target,dp);
         boolean take = false;
@@ -46,9 +46,9 @@ class Solution{
         dp[i][target] = take || nottake?1:0;
         return take || nottake;
     }
-    static Boolean isSubsetSum(int n, int arr[], int target){
-        int[][] dp = new int[n][target+1];
+    static Boolean isSubsetSum(int n, int arr[], int sum){
+        int[][] dp = new int[n][sum+1];
         for(int[] row : dp) Arrays.fill(row,-1);
-        return f(n-1,arr,target,dp);
+        return f(n-1,arr,sum,dp);
     }
 }
